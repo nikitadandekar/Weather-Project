b12b964cdd388f5f4e6c5dc3f9f88c88f058c76d
nikitadandekar <nikitadandekar1720@gmail.com>
2023-06-18 23:39:55 +0530

    Working project

--- src/Components/Left.jsx
+++ src/Components/Left.jsx
@@ -5,9 +5,14 @@ function Left() {
     <div>
       <div className="Container w-96 h-screen bg-indigo-950 ">
         <button className="bg-slate-600 h-9 w-52 mt-11 ml-12 pl-3 text-lg rounded-sm text-slate-50 ">
-          {" "}
           Search for places
         </button>
+        <h1>15 °C</h1>
+        <h1>Shower</h1>
+        <div className="text-slate-500">
+          <p>Today . Fri 5 Jun</p>
+        </div>
+        <div className="text-slate-500">Helsinki</div>
       </div>
     </div>
   );
--- src/Components/Right.jsx
+++ src/Components/Right.jsx
@@ -5,23 +5,67 @@ function Right() {
     <div>
       <div className="Container2 bg-blue-950 w-screen h-screen">
         <div className="flex justify-start gap-6 pl-80 pt-40 ">
-          <div className="w-36 h-52 bg-indigo-950">Tomorrow</div>
-          <div className="w-36 h-52 bg-indigo-950">Sun, 7 Jun</div>
-          <div className="w-36 h-52 bg-indigo-950">Mon, 8 Jun</div>
-          <div className="w-36 h-52 bg-indigo-950">Tue, 9 Jun</div>
-          <div className="w-36 h-52 bg-indigo-950">Wed,10 Jun</div>
+          <div className="w-36 h-52 bg-indigo-950">
+            <h1>Tomorrow</h1>
+            <div className="flex">
+              <p>10°C </p>
+              <p>11°C </p>
+            </div>
+          </div>
+
+          <div className="w-36 h-52 bg-indigo-950">
+            <h1> Sun, 7 Jun</h1>
+            <div className="flex">
+              <p>10°C </p>
+              <p>11°C </p>
+            </div>
+          </div>
+          <div className="w-36 h-52 bg-indigo-950">
+            <h1>Mon, 8 Jun</h1>
+            <div className="flex">
+              <p>10°C </p>
+              <p>11°C </p>
+            </div>
+          </div>
+          <div className="w-36 h-52 bg-indigo-950">
+            <h1> Tue, 9 Jun</h1>
+            <div className="flex">
+              <p>10°C </p>
+              <p>11°C </p>
+            </div>
+          </div>
+          <div className="w-36 h-52 bg-indigo-950">
+            <h1> Wed,10 Jun</h1>
+            <div className="flex">
+              <p>10°C </p>
+              <p>11°C </p>
+            </div>
+          </div>
         </div>
         <h2 className="mt-8 text-slate-400 font-bold from-neutral-100 text-lg ml-80 mb-6">
           Today's Hightligghts
         </h2>
         <div className="">
           <div className="box1 pl-80 flex gap-9 ">
-            <div className="w-96 h-60 bg-indigo-950"></div>
-            <div className="w-96 h-60 bg-indigo-950"></div>
+            <div className="w-96 h-60 bg-indigo-950">
+              <p>Wind Status</p>
+              <h1>7 mph</h1>
+              <p>WSW</p>
+            </div>
+            <div className="w-96 h-60 bg-indigo-950">
+              <p>Humidity</p>
+              <h1>81%</h1>
+            </div>
           </div>
           <div className="box2 pl-80 mt-11 flex gap-9">
-            <div className="w-96 h-40 bg-indigo-950"></div>
-            <div className="w-96 h-40 bg-indigo-950"></div>
+            <div className="w-96 h-40 bg-indigo-950">
+              <p>Visibility</p>
+              <h1>6,4 miles</h1>
+            </div>
+            <div className="w-96 h-40 bg-indigo-950">
+              <p>Air Pressure</p>
+              <h1>998 mb</h1>
+            </div>
           </div>
         </div>
       </div>
